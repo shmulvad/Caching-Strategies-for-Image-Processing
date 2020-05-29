59d9dffd66a1b3dd21b56791e9877c09d0b8e3bc
Søren Hougaard Mulvad <shmulvad@gmail.com>
2020-05-30 01:36:48 +0200

    Update helper_funcs.py

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -1,13 +1,8 @@
 from cachesim import CacheSimulator, Cache, MainMemory
-import matplotlib
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
 import numpy as np
 
-
-MORTON = "morton"
-ROW_ARR = "row_arr"
-BLOCK_ARR = "block_arr"
+MORTON, ROW_ARR, BLOCK_ARR = "morton", "row_arr", "block_arr"
 SIM_CACHE_TIMES = {
     0: {
         'HIT':  0.000001,
@@ -23,20 +18,32 @@ SIM_CACHE_TIMES = {
     },
     3: {
         'HIT':  1.5,
-        'MISS': 1.0 # Miss will never occur for MEM
+        'MISS': 1.0  # Miss will never occur for MEM
     },
 }
 
 
-def save_fig(filename, path="./", should_save=True):
+def save_fig(filename: str, path: str = "./",
+             should_save: bool = True) -> None:
+    """
+    Saves the figure in a tight format with the given filename at path
+    if dictated by should_save
+    """
     if should_save:
         new_filename = path + filename.replace("_", "-")
-        plt.savefig(new_filename, bbox_inches = 'tight')
+        # Add .pdf to file ending if not already part of it
+        filename_splitted = new_filename.split(".")
+        if len(filename_splitted) == 1 or \
+           filename_splitted[-1] not in ["pdf", "png"]:
+            new_filename += ".pdf"
+        plt.savefig(new_filename.lower(), bbox_inches='tight')
 
 
-def make_cache():
+def make_cache() -> CacheSimulator:
+    """Returns a fresh cache of standard size"""
     mem = MainMemory()
-    l3 = Cache("L3", 20480, 16, 64, "LRU")  # 20MB: 20480 sets, 16-ways with cacheline size of 64 bytes
+    l3 = Cache("L3", 20480, 16, 64, "LRU")  # 20MB: 20480 sets, 16-ways with
+                                            # cacheline size of 64 bytes
     mem.load_to(l3)
     mem.store_from(l3)
     l2 = Cache("L2", 512, 8, 64, "LRU", store_to=l3, load_from=l3)  # 256KB
@@ -45,13 +52,23 @@ def make_cache():
     return cs
 
 
-def get_val_arr(results, data_typ, cache_level=0, stat="HIT_count"):
+def get_val_arr(results: dict, data_typ: str,
+                cache_level: int = 0, stat: str = "HIT_count") -> np.ndarray:
+    """
+    Returns an ndarray of the corresponding y-values for all the n-values for
+    a given data_typ, cache_level and stat
+    """
     sorted_res = sorted(results.items(), key=lambda key_val: int(key_val[0]))
     return np.array([float(val[data_typ][cache_level][stat])
                      for _, val in sorted_res])
 
 
-def get_val_arr_time(results, data_typ):
+def get_val_arr_time(results: dict, data_typ: str) -> list:
+    """
+    Returns an array of the simulated cache time for all n-values for a
+    specific data_typ based on the number of hits and misses at different
+    cache levels
+    """
     res_arr = []
     for n in results:
         time_taken = 0.0
@@ -63,9 +80,25 @@ def get_val_arr_time(results, data_typ):
     return res_arr
 
 
-def num_column_and_width_to_displacement(n, width):
+def prettify_name(data_typ: str) -> str:
+    """
+    Returns a pretty version of the data_typ name. If none is found, the
+    data_typ is simply returned
+    """
+    pretty_names = {
+        MORTON: "Morton Ordering",
+        ROW_ARR: "Row Major Array",
+        BLOCK_ARR: "Block Array"
+    }
+    return pretty_names[data_typ] if data_typ in pretty_names else data_typ
+
+
+def num_column_and_width_to_displacement(n: int, width: float = None) \
+                                         -> tuple:
+    if width is None:
+        width = (1.0 / float(n)) * 0.7
     div = n // 2
     arr = [i+0.5 for i in range(-div, div)] \
-          if n % 2 == 0 \
-          else [i for i in range(-div, div+1)]
-    return np.array(arr) * width
+        if n % 2 == 0 \
+        else [i for i in range(-div, div+1)]
+    return np.array(arr) * width, width
