0559b7d5975c5747bd9a542ce8de4eb18185159b
Søren Hougaard Mulvad <shmulvad@gmail.com>
2020-06-05 23:48:04 +0200

    Simplify Morton part_compact in n dim

--- src/data_structures/morton_order.py
+++ src/data_structures/morton_order.py
@@ -1,6 +1,6 @@
 import numpy as np
 from itertools import product
-from typing import Generator, Callable
+from typing import Generator, Callable, Tuple
 from cachesim import CacheSimulator
 
 from helper_funcs import MORTON
@@ -39,7 +39,7 @@ class MortonOrder(CachingDataStructure):
         Sets the correct parting and compact functions to use based on the
         dimension of picture/shape
         """
-        if self.dim <= 4:  # Hardcoded case
+        if self.dim <= 4:  # Hardcoded case which is a little faster
             part_compact_to_use = [
                 (self.identity, self.identity),
                 (self.part1by1, self.compact1by1),
@@ -48,9 +48,9 @@ class MortonOrder(CachingDataStructure):
             ][self.dim - 1]
             self.part_func = part_compact_to_use[0]
             self.compact_func = part_compact_to_use[1]
-        else:  # Dynamic case
-            self.part_func = self.part1byN(self.dim - 1)
-            self.compact_func = self.compact1byN(self.dim - 1)
+        else:  # Dynamic case which works for every n >= 1 but is a bit slower
+            n = self.dim - 1
+            self.part_func, self.compact_func = self.gen_part_compact_funcs(n)
 
     def __get_hex_const__(self, successive_digits: int, spacing: int,
                           num_bits: int = 32) -> int:
@@ -58,10 +58,10 @@ class MortonOrder(CachingDataStructure):
         Returns the hexadecimal constant to be used for bit parting for a given
         number of succesive digits and spacing inbetween
 
-        >>> bin(get_hex_const(2, 3, 16))
-        '0b1000110001100011'
-        >>> bin(get_hex_const(1, 4, 16))
-        '0b1000010000100001'
+        >>> get_hex_const(2, 3, 16)
+        0b1000110001100011
+        >>> get_hex_const(1, 4, 16)
+        0b1000010000100001
         """
         hex_num, bit_idx = 0, 0
         while bit_idx < num_bits:
@@ -103,8 +103,12 @@ class MortonOrder(CachingDataStructure):
         """
         return self.morton_encode(args)
 
-    def part1byN(self, n: int, num_bits: int = 32) -> Callable[[int], int]:
-        """Returns a partbyN function for a given n and number of bits"""
+    def gen_part_compact_funcs(self, n: int, num_bits: int = 32) \
+            -> Tuple[Callable[[int], int], Callable[[int], int]]:
+        """
+        Returns a tuple of part1byN and compact1byN functions for a given n
+        and number of bits
+        """
         # num_iters should idealy be set more carefully to avoid
         # unneeded computing
         num_iters = 5 * (num_bits // 32)
@@ -112,16 +116,23 @@ class MortonOrder(CachingDataStructure):
         hex_consts = [self.__get_hex_const__(2**i, ns[i], num_bits)
                       for i in range(num_iters)]
 
-        def part1byN_func(x: int) -> int:
+        def part1byN(x: int) -> int:
             x &= hex_consts[-1]
             for i in range(num_iters - 2, -1, -1):
                 x = (x | (x << ns[i])) & hex_consts[i]
             return x
 
-        return part1byN_func
+        def compact1byN(x: int) -> int:
+            x &= hex_consts[0]
+            for i in range(1, num_iters):
+                x = (x | (x >> ns[i-1])) & hex_consts[i]
+            return x
+
+        return part1byN, compact1byN
 
     def part1by3(self, x: int) -> int:
         """Inserts three 0 bits after each of the 8 low bits of x"""
+        # Modified from http://asgerhoedt.dk/?p=276
         x &= 0x000000ff                   # x = ---- ---- ---- ---- ---- ---- 7654 3210
         x = (x | (x << 12)) & 0x000f000f  # x = ---- ---- ---- 7654 ---- ---- ---- 3210
         x = (x | (x <<  6)) & 0x03030303  # x = ---- --76 ---- --54 ---- --32 ---- --10
@@ -130,6 +141,7 @@ class MortonOrder(CachingDataStructure):
 
     def part1by2(self, x: int) -> int:
         """Inserts two 0 bits after each of the 11 low bits of x"""
+        # Modified from http://asgerhoedt.dk/?p=276
         x &= 0x000007ff                   # x = ---- ---- ---- ---- ---- -a98 7654 3210
         x = (x | (x << 16)) & 0x070000ff  # x = ---- -a98 ---- ---- ---- ---- 7654 3210
         x = (x | (x <<  8)) & 0x0700f00f  # x = ---- -a98 ---- ---- 7654 ---- ---- 3210
@@ -139,6 +151,7 @@ class MortonOrder(CachingDataStructure):
 
     def part1by1(self, x: int) -> int:
         """Inserts one 0 bit after each of the 16 low bits of x"""
+        # Modified from http://asgerhoedt.dk/?p=276
         x &= 0x0000ffff                  # x = ---- ---- ---- ---- fedc ba98 7654 3210
         x = (x | (x << 8)) & 0x00ff00ff  # x = ---- ---- fedc ba98 ---- ---- 7654 3210
         x = (x | (x << 4)) & 0x0f0f0f0f  # x = ---- fedc ---- ba98 ---- 7654 ---- 3210
@@ -159,27 +172,11 @@ class MortonOrder(CachingDataStructure):
             val |= self.part_func(key[i]) << i
         return val
 
-    def compact1byN(self, n: int, num_bits: int = 32) -> Callable[[int], int]:
-        """Returns a compact1byN function for a given n and number of bits"""
-        # num_iters should idealy be set more carefully to avoid
-        # unneeded computation
-        num_iters = 5 * (num_bits // 32)
-        ns = [n * 2**i for i in range(num_iters)]
-        hexs = [self.__get_hex_const__(2**i, ns[i], num_bits)
-                for i in range(num_iters)]
-
-        def compact1byN_func(x: int) -> int:
-            x &= hexs[0]
-            for i in range(1, num_iters):
-                x = (x | (x >> ns[i-1])) & hexs[i]
-            return x
-
-        return compact1byN_func
-
     def compact1by3(self, x: int) -> int:
         """
         Inverse of 'part1by3'. Removes all bits not at indices divisible by 4
         """
+        # Modified based on http://asgerhoedt.dk/?p=276
         x &= 0x11111111                   # x = ---7 ---6 ---5 ---4 ---3 ---2 ---1 ---0
         x = (x | (x >>  3)) & 0x03030303  # x = ---- --76 ---- --54 ---- --32 ---- --10
         x = (x | (x >>  6)) & 0x000f000f  # x = ---- ---- ---- 7654 ---- ---- ---- 3210
@@ -190,6 +187,7 @@ class MortonOrder(CachingDataStructure):
         """
         Inverse of 'part1by2'. Removes all bits not at indices divisible by 3
         """
+        # Modified based on http://asgerhoedt.dk/?p=276
         x &= 0x09249249                   # x = ---- 9--8 --7- -6-- 5--4 --3- -2-- 1--0
         x = (x | (x >>  2)) & 0x030c30c3  # x = ---- --98 ---- 76-- --54 ---- 32-- --10
         x = (x | (x >>  4)) & 0x0300f00f  # x = ---- --98 ---- ---- 7654 ---- ---- 3210
@@ -201,6 +199,7 @@ class MortonOrder(CachingDataStructure):
         """
         Inverse of 'part1by1'. Removes all bits not at indices divisible by 2
         """
+        # Modified based on http://asgerhoedt.dk/?p=276
         x &= 0x55555555                  # x = -f-e -d-c -b-a -9-8 -7-6 -5-4 -3-2 -1-0
         x = (x | (x >> 1)) & 0x33333333  # x = --fe --dc --ba --98 --76 --54 --32 --10
         x = (x | (x >> 2)) & 0x0f0f0f0f  # x = ---- fedc ---- ba98 ---- 7654 ---- 3210
