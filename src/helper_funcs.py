f8e3726fabe7779a0a1947a4d18d298bd2a2294a
Søren Hougaard Mulvad <shmulvad@gmail.com>
2020-06-06 00:07:57 +0200

    Update helper_funcs with table functions

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -3,31 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 MORTON, ROW_ARR, BLOCK_ARR = "morton", "row_arr", "block_arr"
-SIM_CACHE_TIMES = {
-    0: {
-        'HIT':  0.000001,
-        'MISS': 0.0001
-    },
-    1: {
-        'HIT':  0.0001,
-        'MISS': 0.01
-    },
-    2: {
-        'HIT':  0.01,
-        'MISS': 1.0
-    },
-    3: {
-        'HIT':  1.5,
-        'MISS': 1.0  # Miss will never occur for MEM
-    },
-}
 
 
 def save_fig(filename: str, path: str = "./",
              should_save: bool = True) -> None:
     """
     Saves the figure in a tight format with the given filename at path
-    if dictated by should_save
+    if dictated by should_save.
     """
     if should_save:
         new_filename = path + filename.replace("_", "-")
@@ -40,7 +22,7 @@ def save_fig(filename: str, path: str = "./",
 
 
 def make_cache() -> CacheSimulator:
-    """Returns a fresh cache of standard size"""
+    """Returns a fresh cache of standard size."""
     mem = MainMemory()
     l3 = Cache("L3", 20480, 16, 64, "LRU")  # 20MB: 20480 sets, 16-ways with
                                             # cacheline size of 64 bytes
@@ -56,34 +38,17 @@ def get_val_arr(results: dict, data_typ: str,
                 cache_level: int = 0, stat: str = "HIT_count") -> np.ndarray:
     """
     Returns an ndarray of the corresponding y-values for all the n-values for
-    a given data_typ, cache_level and stat
+    a given data_typ, cache_level and stat.
     """
     sorted_res = sorted(results.items(), key=lambda key_val: int(key_val[0]))
     return np.array([float(val[data_typ][cache_level][stat])
                      for _, val in sorted_res])
 
 
-def get_val_arr_time(results: dict, data_typ: str) -> list:
-    """
-    Returns an array of the simulated cache time for all n-values for a
-    specific data_typ based on the number of hits and misses at different
-    cache levels
-    """
-    res_arr = []
-    for n in results:
-        time_taken = 0.0
-        for cache_level in range(len(results[n][data_typ])):
-            for typ in ['HIT', 'MISS']:
-                count = results[n][data_typ][cache_level][f"{typ}_count"]
-                time_taken += count * SIM_CACHE_TIMES[cache_level][typ]
-        res_arr.append(time_taken)
-    return res_arr
-
-
 def prettify_name(data_typ: str) -> str:
     """
     Returns a pretty version of the data_typ name. If none is found, the
-    data_typ is simply returned
+    data_typ is simply returned.
     """
     pretty_names = {
         MORTON: "Morton Ordering",
@@ -93,12 +58,30 @@ def prettify_name(data_typ: str) -> str:
     return pretty_names[data_typ] if data_typ in pretty_names else data_typ
 
 
-def num_column_and_width_to_displacement(n: int, width: float = None) \
-                                         -> tuple:
-    if width is None:
-        width = (1.0 / float(n)) * 0.7
-    div = n // 2
-    arr = [i+0.5 for i in range(-div, div)] \
-        if n % 2 == 0 \
-        else [i for i in range(-div, div+1)]
-    return np.array(arr) * width, width
+def print_table_nums(dim: int, results: dict, ns: list, stat: str) -> None:
+    """
+    Prints a stat (i.e. HIT_count) for morton ordering and block array
+    relative to row-major array. Is printed in a format that is copy-pasteable
+    on a row basis to a LaTeX-table.
+    """
+    print(f"Dim: {dim}\n{'ns':10}: {ns}")
+    row_arr_res = get_val_arr(results, ROW_ARR, stat=stat)
+    for data_typ in [MORTON, BLOCK_ARR]:
+        rel_res = get_val_arr(results, data_typ, stat=stat) / row_arr_res
+        rel_res_strs = map(lambda val: f"{val:.2f}", rel_res)
+        print(f"{data_typ:10}: {' & '.join(rel_res_strs)}")
+    print("\n")
+
+
+def hit_to_misses(dim: int, results: dict, ns: list) -> None:
+    """
+    Prints the hit-to-miss ratio for the three data structures. Is printed in
+    a format that is copy-pasteable on a row basis to a LaTeX-table.
+    """
+    print(f"Dim: {dim}\n{'ns':10}: {ns}")
+    for data_typ in [MORTON, ROW_ARR, BLOCK_ARR]:
+        hits = get_val_arr(results, data_typ, stat="HIT_count")
+        misses = get_val_arr(results, data_typ, stat="MISS_count")
+        hit_to_misses = map(lambda val: f"{val:.2f}", hits / misses)
+        print(f"{data_typ:10}: {' & '.join(hit_to_misses)}")
+    print("\n")
